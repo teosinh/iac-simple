27f720852da1847ba6d15cf73dd511744eeb3128
teosinh <52597480+teosinh@users.noreply.github.com>
2022-02-22 20:44:07 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,6 @@ terraform {
 
 provider "aws" {
   region = "ap-southeast-1"
-  access_key = "AKIA6KHACDI5U4XJ3CEZ"
-  secret_key = "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"
 }
 
 resource "aws_s3_bucket" "hien-test-s3" {
